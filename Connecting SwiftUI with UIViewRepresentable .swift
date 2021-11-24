d84f536ffa03b39f8a0100025714398eef594cd8
Rion Hasolli <91853769+RionHasolli1@users.noreply.github.com>
2021-11-24 12:29:12 +0100

    Create Connecting SwiftUI with UIViewRepresentable .swift

---
+++ Connecting SwiftUI with UIViewRepresentable .swift
@@ -0,0 +1,58 @@
+struct toogle1:View{
+    @State private var dhezu=false
+    
+    
+    var body: some View{
+        
+        
+       
+        VStack{
+            
+            List{
+         
+                
+                Toggle("hihi",isOn:$dhezu)
+                    .toggleStyle(SwitchToggleStyle(tint: .green))
+               ActivityIndicator()
+                
+                .disabled(dhezu)
+        
+        }
+        
+        
+            .listStyle(GroupedListStyle())
+        
+        
+        }
+    }
+        
+    
+}
+
+
+struct ActivityIndicator: UIViewRepresentable {
+    typealias Context = UIViewRepresentableContext<Self>
+    typealias UIViewType = UIActivityIndicatorView
+
+    public func makeUIView(context: Context) -> UIViewType {
+        UIActivityIndicatorView(style: .medium)
+    }
+
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+        if !context.environment.isEnabled && uiView.isAnimating {
+            uiView.stopAnimating()
+        }
+        if context.environment.isEnabled &&  !uiView.isAnimating {
+            uiView.startAnimating()
+        }
+    }
+}
+    
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+       toogle1()
+      
+      
+    }
+}
